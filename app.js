08a0b50b8514c3ea958279e5b1e0e11e7e3029be
Johanna Andén <johannaanden@Johannas-MacBook-Pro.local>
2018-11-27 06:51:42 +0100

    Adds variable scoreBoard_div and sets value

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ let userScore = 0;
 let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
-
+const scoreBoard_div = document.querySelector("score-board");
