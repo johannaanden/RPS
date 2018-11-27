a5c557040b6ae48ba6f6dcd60052e63d766f7c25
Johanna Andén <johannaanden@Johannas-MacBook-Pro.local>
2018-11-27 06:59:13 +0100

    Creates main function

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissor");
+
+function main() {
+    rock_div.addEventListener('click', () => game("rock"));
+    paper_div.addEventListener('click', () => game("paper"));
+    scissors_div.addEventListener('click', () => game("scissor"));
+}
