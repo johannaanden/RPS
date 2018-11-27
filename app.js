53c7cf26916cffe8a9356d4d8051fb4a51d6b163
Johanna Andén <johannaanden@Johannas-MacBook-Pro.local>
2018-11-27 06:52:51 +0100

    Adds variable paper_div and sets value

--- app.js
+++ app.js
@@ -5,3 +5,4 @@ const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector("score-board");
 const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
