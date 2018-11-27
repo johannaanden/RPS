82f691be7a75962ee152d7a19fa46366aa063646
Johanna Andén <johannaanden@Johannas-MacBook-Pro.local>
2018-11-27 06:52:29 +0100

    Adds variable rock_div and sets value

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector("score-board");
 const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("rock");
