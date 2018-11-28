6041d3deaa8e99c76f96959a60e0fbdc9101e2af
johannaanden <johannaanden@hotmail.com>
2018-11-28 07:57:04 +0100

    Changes variable UserWord to userWord

--- app.js
+++ app.js
@@ -14,25 +14,25 @@ function getComputerOption() {
 }
 
 function win(userOption, computerOption) {
-    const UserWord = "You picked ";
+    const userWord = "You picked ";
     const ComputerWord = " that computer picked";
     const userOption_div = document.getElementById(userOption);
     userScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = `You win! 🏆 ${UserWord} ${userOption} that beats ${computerOption}${ComputerWord}.`;
+    result_p.innerHTML = `You win! 🏆 ${userWord} ${userOption} that beats ${computerOption}${ComputerWord}.`;
     userOption_div.classList.add('green-glow');
     setTimeout(() =>  userOption_div.classList.remove('green-glow'), 1000);
 }
 
 function lose(userOption, computerOption) {
-    const UserWord = "You picked ";
+    const userWord = "You picked ";
     const ComputerWord = " that computer picked";
     const userOption_div = document.getElementById(userOption);
     computerScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = `You lost! 😭 ${UserWord} ${userOption} that loses to ${computerOption}${ComputerWord}.`;
+    result_p.innerHTML = `You lost! 😭 ${userWord} ${userOption} that loses to ${computerOption}${ComputerWord}.`;
     userOption_div.classList.add('red-glow');
     setTimeout(() => userOption_div.classList.remove('red-glow'), 1000);
 }
